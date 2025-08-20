06001847480c4bb62cdeaf051d3d0911231e6c5e
mminseop <alstjq903@naver.com>
2025-08-20 11:27:03 +0900

    ✨ Feat: title 아래 git-actions 문구 추가

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>mminseop-bucket</h1>
+      <h2>git-actions</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
